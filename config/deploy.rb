8864cd81b706bc92fb2d030fd4175e67fc29b36b
srinivas <srinivas4ruby@gmail.com>
2016-06-18 18:14:54 +0530

    change ip in database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :puma_workers, 0
 set :pty,             true
 set :use_sudo,        true # changed false to true
 set :stage,           :production
-set :deploy_via,      :remote_cache
+set :deploy_via,      :copy #remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
