9c2d4ab42a680506beacfe40aa2cd78f813f860a
srinivas <srinivas4ruby@gmail.com>
2016-06-18 16:46:53 +0530

    comment set :ssh_options in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-server '139.59.1.199', port: 30000, roles: [:web, :app, :db], primary: true
+server '139.59.1.199', port: 80, roles: [:web, :app, :db], primary: true
 set :application, 'todoapp_capistrano'
-set :repo_url, 'git@github.com:ruby4srinivas/todoapp_repository.git'
+set :repo_url, 'git@github.com:ruby4srinivas/todoapp_capistrano.git'
 set :user, 'demo'
 set :puma_threads, [4,16]
 set :puma_workers, 0
@@ -18,7 +18,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+#set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
